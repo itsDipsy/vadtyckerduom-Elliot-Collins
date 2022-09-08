6907c4e9ee1d8e0749121201332626fd99d929f7
itsDipsy <elliot.collinszcv@gmail.com>
2022-09-08 15:38:44 +0200

    all of it done

---
+++ scripts.js
@@ -0,0 +1,6 @@
+
+let button = document.getElementsByClassName("btn-container-Wes");
+button[0].addEventListener("click", () => {
+    console.log("it worked");
+    window.location.href = "DONOTOPEN.html";
+});
